5be8aeb83c79e0a2c8310054aa0f43641a24e81d
VisalamMurugan <visalam.murugan@gmail.com>
2020-06-23 10:17:13 +0530

    Added and commited "Add_Sentence Branch" V1.0

--- SRC/HelloWorld.java
+++ SRC/HelloWorld.java
@@ -4,6 +4,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World-June 2020!");
+		System.out.println("Hello World-Add sentence branch - June 2020!");
 	}
 }
