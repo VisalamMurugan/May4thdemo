6aa807d1cfffa66c092e26cfd8d0386d4599d2cd
VisalamMurugan <visalam.murugan@gmail.com>
2020-06-23 10:55:34 +0530

    Update HelloWorld.java

--- SRC/HelloWorld.java
+++ SRC/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld
 	public static void main(String[] args) {
 
 		System.out.println("Hello World-June 2020!");
-		System.out.println("Hello World.java!");
+		System.out.println("Hello WorldAAA.java!");
 
 		System.out.println("Hello World-Add sentence branch - June 2020 done!!!!");
 
